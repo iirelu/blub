d66017b72c18fae0c3408ca4e8be4e84e5ae6fb3
iirelu <totallymyrealemail@hush.ai>
2014-01-22 13:46:59 +0000

    added velocity

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var width = 500, height = 400;
 
-var stage = new PIXI.Stage(0xCF7FC0);
+var stage = new PIXI.Stage(0xAB99EF);
 var renderer = PIXI.autoDetectRenderer(width, height);
 document.getElementById("container").appendChild(renderer.view);
 
@@ -9,6 +9,7 @@ var blub = PIXI.Sprite.fromImage("blub.png");
 blub.anchor.x = 0.5;
 blub.anchor.y = 0.5;
 blub.rotation = 0;
+blub.velocity = {x: 0, y: 0};
 
 var startTime = new Date().getTime();
 
@@ -21,8 +22,11 @@ function draw() {
   var mouse = stage.getMousePosition();
 
   blub.rotation = Math.atan((blub.position.y-mouse.y)/(blub.position.x-mouse.x));
-  blub.position.x = (blub.position.x*5 + mouse.x)/6;
-  blub.position.y = (blub.position.y*5 + mouse.y)/6;
+  blub.velocity.x = (blub.velocity.x*5 + mouse.x - blub.position.x)/6;
+  blub.velocity.y = (blub.velocity.y*5 + mouse.y - blub.position.y)/6;
+
+  blub.position.x += blub.velocity.x/5;
+  blub.position.y += blub.velocity.y/5;
   
   var dist = Math.sqrt(Math.pow(blub.position.x-mouse.x, 2) + Math.pow(blub.position.y-mouse.y, 2));
   audio.changeFrequency(dist*5);
